b6f10aad06aff902595c297b93bb00001757648c
Sulemio <rcskuroi@gmail.com>
2024-05-18 14:43:23 -0300

    add categorias

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -161,36 +161,20 @@
         <h2 class="text-2xl font-medium text-gray-800 uppercase mb-6">Compre por Categoria</h2>
         <div class="grid grid-cols-3 gap-3">
             <div class="relative rounded-sm overflow-hidden group">
-                <img src="assets/images/category/category-1.jpg" alt="category 1" class="w-full">
+                <img src="assets/images/category/category-1.webp" alt="category 1" class="h-full">
                 <a href="#"
-                    class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Bedroom</a>
+                    class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Carnes</a>
             </div>
             <div class="relative rounded-sm overflow-hidden group">
-                <img src="assets/images/category/category-2.jpg" alt="category 1" class="w-full">
+                <img src="assets/images/category/category-2.webp" alt="category 1" class="h-full">
                 <a href="#"
-                    class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Mattrass</a>
+                    class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Vegetais</a>
             </div>
             <div class="relative rounded-sm overflow-hidden group">
-                <img src="assets/images/category/category-3.jpg" alt="category 1" class="w-full">
+                <img src="assets/images/category/category-3.webp" alt="category 1" class="h-full">
                 <a href="#"
-                    class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Outdoor
+                    class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Bebidas
                 </a>
-            </div>
-            <div class="relative rounded-sm overflow-hidden group">
-                <img src="assets/images/category/category-4.jpg" alt="category 1" class="w-full">
-                <a href="#"
-                    class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Sofa</a>
-            </div>
-            <div class="relative rounded-sm overflow-hidden group">
-                <img src="assets/images/category/category-5.jpg" alt="category 1" class="w-full">
-                <a href="#"
-                    class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Living
-                    Room</a>
-            </div>
-            <div class="relative rounded-sm overflow-hidden group">
-                <img src="assets/images/category/category-6.jpg" alt="category 1" class="w-full">
-                <a href="#"
-                    class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Kitchen</a>
             </div>
         </div>
     </div>
