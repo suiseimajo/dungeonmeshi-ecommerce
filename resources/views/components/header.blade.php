6cc052595cee38a208cc410f05884dffcc8de1d6
miorembran <67882285+suiseimajo@users.noreply.github.com>
2024-05-24 15:55:22 -0300

    header e autenticação

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -68,4 +68,61 @@
             </div>
         </div>
     </header>
+    <!-- navbar -->
+    <nav class="bg-gray-800">
+        <div class="container flex">
+            <div class="px-8 py-4 bg-primary flex items-center cursor-pointer relative group">
+                <span class="text-white">
+                    <i class="fa-solid fa-bars"></i>
+                </span>
+                <span class="capitalize ml-2 text-white">Categorias</span>
+
+                <!-- dropdown -->
+                <div class="absolute w-full left-0 top-full bg-white shadow-md py-3 divide-y divide-gray-300 divide-dashed opacity-0 group-hover:opacity-100 transition duration-300 invisible group-hover:visible">
+                    <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
+                        <img src="../assets/images/icons/sofa.svg" alt="sofa" class="w-5 h-5 object-contain">
+                        <span class="ml-6 text-gray-600 text-sm">Sofa</span>
+                    </a>
+                    <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
+                        <img src="../assets/images/icons/terrace.svg" alt="terrace" class="w-5 h-5 object-contain">
+                        <span class="ml-6 text-gray-600 text-sm">Terarce</span>
+                    </a>
+                    <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
+                        <img src="../assets/images/icons/bed.svg" alt="bed" class="w-5 h-5 object-contain">
+                        <span class="ml-6 text-gray-600 text-sm">Bed</span>
+                    </a>
+                    <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
+                        <img src="../assets/images/icons/office.svg" alt="office" class="w-5 h-5 object-contain">
+                        <span class="ml-6 text-gray-600 text-sm">office</span>
+                    </a>
+                    <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
+                        <img src="../assets/images/icons/outdoor-cafe.svg" alt="outdoor" class="w-5 h-5 object-contain">
+                        <span class="ml-6 text-gray-600 text-sm">Outdoor</span>
+                    </a>
+                    <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
+                        <img src="../assets/images/icons/bed-2.svg" alt="Mattress" class="w-5 h-5 object-contain">
+                        <span class="ml-6 text-gray-600 text-sm">Mattress</span>
+                    </a>
+                </div>
+            </div>
+
+            <div class="flex items-center justify-between flex-grow pl-12">
+                <div class="flex items-center space-x-6 capitalize">
+                    <a href="{{ route('home') }}" class="text-gray-200 hover:text-white transition">Início</a>
+                    <a href="#" class="text-gray-200 hover:text-white transition">Comprar</a>
+                    <a href="#" class="text-gray-200 hover:text-white transition">Sobre Nós</a>
+                    <a href="#" class="text-gray-200 hover:text-white transition">Contato</a>
+                </div>
+            @if(Auth::check())
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <a href="profile" class="text-gray-200 hover:text-white transition" onclick="event.preventDefault(); this.closest('form').submit();">Sair</a>
+                </form>
+            @else
+                <a href="profile" class="text-gray-200 hover:text-white transition">Entrar</a>
+            @endif
+            </div>
+        </div>
+    </nav>
+    <!-- ./navbar -->
 </body>
