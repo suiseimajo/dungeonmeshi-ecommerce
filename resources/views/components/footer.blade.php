c9da56b9d98551a27d2670cc1f42f01bcb0deaec
miorembran <67882285+suiseimajo@users.noreply.github.com>
2024-06-01 15:08:02 -0300

    update login e home

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -22,50 +22,5 @@
             </div>
         </div>
         <br>
-
-        <div class="col-span-2 grid grid-cols-2 gap-4">
-            <div class="grid grid-cols-2 gap-4 md:gap-8">
-                <div>
-                    <h3 class="text-sm font-semibold text-gray-400 uppercase tracking-wider">Solutions</h3>
-                    <div class="mt-4 space-y-4">
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Marketing</a>
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Analitycs</a>
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Commerce</a>
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Insights</a>
-                    </div>
-                </div>
-
-                <div>
-                    <h3 class="text-sm font-semibold text-gray-400 uppercase tracking-wider">Support</h3>
-                    <div class="mt-4 space-y-4">
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Pricing</a>
-                        <!-- <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Documentation</a> -->
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Guides</a>
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">API Status</a>
-                    </div>
-                </div>
-            </div>
-            <div class="grid grid-cols-2 gap-8">
-                <div>
-                    <h3 class="text-sm font-semibold text-gray-400 uppercase tracking-wider">Solutions</h3>
-                    <div class="mt-4 space-y-4">
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Marketing</a>
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Analitycs</a>
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Commerce</a>
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Insights</a>
-                    </div>
-                </div>
-
-                <div>
-                    <h3 class="text-sm font-semibold text-gray-400 uppercase tracking-wider">Support</h3>
-                    <div class="mt-4 space-y-4">
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Pricing</a>
-                        <!-- <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Documentation</a> -->
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">Guides</a>
-                        <a href="#" class="text-base text-gray-500 hover:text-gray-900 block">API Status</a>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </footer>
