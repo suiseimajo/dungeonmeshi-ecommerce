973b54b3f1abac174f35e42b64aa18fde0e9fecb
miorembran <67882285+suiseimajo@users.noreply.github.com>
2024-07-17 14:07:25 -0300

    avaliação na home

--- resources/views/livewire/product-list.blade.php
+++ resources/views/livewire/product-list.blade.php
@@ -32,28 +32,21 @@
                     </div>
                     <div class="flex items-center">
                 <div class="flex gap-1 fa-1x">
-                <button type="button" wire:click="saveRate('{{$product->id}}')" wire:confirm="Avaliar esse produto?">
-                    <label for="star1">
-                        <input class="hidden" wire:model.live="rating" type="radio" id="star1" name="rating" value="1" />
-                        <i class="cursor-pointer @if($product->ratings->avg('rating') >= 1) text-yellow-400 @endif fa-solid fa-star"></i>
+                    <label>
+                        <i class="@if($product->ratings->avg('rating') >= 1 ) text-yellow-400 @endif fa-solid fa-star"></i>
                     </label>
-                    <label for="star2">
-                        <input class="hidden" wire:model.live="rating" type="radio" id="star2" name="rating" value="2" />
-                        <i class="cursor-pointer @if($product->ratings->avg('rating') >= 2) text-yellow-400 @endif fa-solid fa-star"></i>
+                    <label>
+                        <i class="@if($product->ratings->avg('rating') >= 2 ) text-yellow-400 @endif fa-solid fa-star"></i>
                     </label>
-                    <label for="star3">
-                        <input class="hidden" wire:model.live="rating" type="radio" id="star3" name="rating" value="3" />
-                        <i class="cursor-pointer @if($product->ratings->avg('rating') >= 3) text-yellow-400 @endif fa-solid fa-star"></i>
+                    <label>
+                        <i class="@if($product->ratings->avg('rating') >= 3 ) text-yellow-400 @endif fa-solid fa-star"></i>
                     </label>
-                    <label for="star4">
-                        <input class="hidden" wire:model.live="rating" type="radio" id="star4" name="rating" value="4" />
-                        <i class="cursor-pointer @if($product->ratings->avg('rating') >= 4) text-yellow-400 @endif fa-solid fa-star"></i>
+                    <label>
+                        <i class="@if($product->ratings->avg('rating') >= 4 ) text-yellow-400 @endif fa-solid fa-star"></i>
                     </label>
-                    <label for="star5">
-                        <input class="hidden" wire:model.live="rating" type="radio" id="star5" name="rating" value="5" />
-                        <i class="cursor-pointer @if($product->ratings->avg('rating') >= 5) text-yellow-400 @endif fa-solid fa-star"></i>
+                    <label>
+                        <i class="@if($product->ratings->avg('rating') >= 5 ) text-yellow-400 @endif fa-solid fa-star"></i>
                     </label>
-                </button>
                 </div>
                 <div class="text-xs text-gray-500 ml-3">{{count($product->ratings)}}</div>
             </div>
